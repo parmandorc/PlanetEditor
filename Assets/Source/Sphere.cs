29a5c479e775f579aab98d9547ae28fc170d98d4
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-26 18:50:22 +0100

    Created Sphere class
    Still doesn’t provide a spheric mesh. Next commits will focus on that.

---
+++ Assets/Source/Sphere.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Sphere : MonoBehaviour 
+{
+	// The mesh of the sphere
+	private Mesh mesh;
+
+	void Awake() 
+	{
+		// Create the mesh component
+		mesh = new Mesh();
+		mesh.name = gameObject.name + "_mesh";
+		GetComponent<MeshFilter>().mesh = mesh;
+	}
+
+	void Start()
+	{
+		// Create mesh vertices
+		Vector3[] vertices = {
+			new Vector3( 1.0f, 0.0f,  1.0f),
+			new Vector3( 1.0f, 0.0f, -1.0f),
+			new Vector3(-1.0f, 0.0f, -1.0f),
+			new Vector3(-1.0f, 0.0f,  1.0f)
+		};
+		mesh.vertices = vertices;
+
+		// Create mesh triangles
+		int[] triangles = {
+			0, 1, 2,
+			2, 3, 0
+		};
+		mesh.triangles = triangles;
+	}
+}
