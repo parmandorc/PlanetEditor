902d4a446937063eb879441f90e58c5bcc6e1e29
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-26 19:55:27 +0100

    Added recursive subdivision for sphere creation
    Implemented the algorithm that recursively subdivides the mesh by
    dividing each triangle into 4 using each edge’s middle point.

--- Assets/Source/Sphere.cs
+++ Assets/Source/Sphere.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Sphere : MonoBehaviour 
 {
+	[SerializeField]
+	private uint RecursionLevel;
+
 	// The mesh of the sphere
 	private Mesh mesh;
 
@@ -19,6 +22,12 @@ public class Sphere : MonoBehaviour
 	void Start()
 	{
 		CreateIcosahedron();
+
+		// Create sphere via recursive subdivision
+		for (int i = 0; i < RecursionLevel; i++)
+		{
+			SubdivideMesh();
+		}
 	}
 
 	void CreateIcosahedron()
@@ -73,5 +82,50 @@ public class Sphere : MonoBehaviour
 
 		// Since the mesh is centered in the origin, the normals are equal to the vectors that are used to create the vertices.
 		mesh.normals = vertices;
+	}
+
+	// Subdivides the current mesh by dividing each triangle into 4 triangles from each edge's middlepoint.
+	void SubdivideMesh()
+	{
+		List<Vector3> newVertices = new List<Vector3>(mesh.vertices);
+		List<int> newTriangles = new List<int>();
+
+		// Create a cache for middle points, so no duplicate vertices are created
+		Dictionary<Vector2, int> cache = new Dictionary<Vector2, int>();
+
+		for (int i = 0; i < mesh.triangles.Length; i += 3)
+		{
+			// Create triangle of middle points
+			for (int j = 0; j < 3; j++)
+			{
+				Vector2 key = new Vector2(mesh.triangles[i + j], mesh.triangles[i + (j + 1) % 3]);
+				if (key.x > key.y)
+					key = new Vector2 (key.y, key.x); // Make key canonic: always smaller index first
+
+				if (!cache.ContainsKey(key))
+				{
+					// Calculate middle point
+					newVertices.Add(((newVertices[mesh.triangles[i + j]] + newVertices[mesh.triangles[i + (j + 1) % 3]]) * 0.5f).normalized);
+					cache.Add(key, newVertices.Count - 1);
+				}
+
+				newTriangles.Add(cache[key]);
+			}
+
+			// Create the other three triangles
+			int a = newTriangles[newTriangles.Count - 3];
+			int b = newTriangles[newTriangles.Count - 2];
+			int c = newTriangles[newTriangles.Count - 1];
+			newTriangles.AddRange(new int[] {
+				mesh.triangles[i], a, c,
+				mesh.triangles[i + 1], b, a,
+				mesh.triangles[i + 2], c, b
+			});
+		}
+
+		// Assign new data to mesh
+		mesh.vertices = newVertices.ToArray();
+		mesh.triangles = newTriangles.ToArray();
+		mesh.normals = newVertices.ToArray();
 	}
 }
