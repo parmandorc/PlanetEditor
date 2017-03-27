be4fdbddc0726516ebc19e46833ea551a9cbd231
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-27 20:49:33 +0100

    Added more wave data
    Completed wave data xml file to have 8 sets

--- Assets/Source/TerrainGenerator.cs
+++ Assets/Source/TerrainGenerator.cs
@@ -29,7 +29,7 @@ public class TerrainGenerator : MonoBehaviour
 		// Determine the height of each point
 		for (int i = 0; i < points.Length; i++)
 		{
-			heights[i] = 0.9f + Noise.FractalPerlin3D(seedOffset + points[i], 1.0f, 5, 2.0f, 0.5f, 1.0f, 0.0f) * 0.75f;
+			heights[i] = 0.875f + Noise.FractalPerlin3D(seedOffset + points[i], 1.0f, 5, 2.0f, 0.5f, 1.0f, 0.0f) * 0.75f;
 		}
 
 		sphere.SetRadiuses(heights);
