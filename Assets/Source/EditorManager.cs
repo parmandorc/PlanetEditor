596fc0155f132c68599251b5e92f0090540e80d1
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-27 18:20:05 +0100

    Added UI for rendering mode

---
+++ Assets/Source/EditorManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorManager : MonoBehaviour
+{
+	[SerializeField]
+	private Sphere sphere;
+
+	// Render mode
+	public void SetDefaultRenderMode() { sphere.SetRenderMode (Sphere.RenderMode.Default); }
+	public void SetSolidRenderMode() { sphere.SetRenderMode (Sphere.RenderMode.Solid); }
+	public void SetGradientRenderMode() { sphere.SetRenderMode (Sphere.RenderMode.Gradient); }
+}
