6e84fcb4e459e09946560b564ee86e61630105f8
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-28 16:15:52 +0100

    Added UI display toggle

---
+++ Assets/Source/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For now, this only manages miscellaneous player input
+public class Player : MonoBehaviour
+{	
+	[SerializeField]
+	public GameObject canvas;
+
+	void Update ()
+	{
+		if (Input.GetButtonDown("ToggleUI"))
+		{
+			canvas.SetActive(!canvas.activeSelf);
+		}
+	}
+}
