a199858de3790e49943e89a48eaf541b8e9dfb5e
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-28 10:21:48 +0100

    Added camera orbit around planet

---
+++ Assets/Source/OrbitAroundTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAroundTarget : MonoBehaviour 
+{
+	[SerializeField]
+	private GameObject target;
+
+	[SerializeField]
+	private float speed;
+
+	void Update () 
+	{
+		transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * speed);
+		transform.LookAt(target.transform);
+	}
+}
