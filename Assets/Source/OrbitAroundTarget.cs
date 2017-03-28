e29f2cee2801321c521cabe44bdf02a44de16842
Pablo A. Rodríguez Codes <parmandorc@gmail.com>
2017-03-28 16:07:34 +0100

    Added navigation controls

--- Assets/Source/OrbitAroundTarget.cs
+++ Assets/Source/OrbitAroundTarget.cs
@@ -8,11 +8,45 @@ public class OrbitAroundTarget : MonoBehaviour
 	private GameObject target;
 
 	[SerializeField]
-	private float speed;
+	private float autoSpeed;
+
+	[SerializeField]
+	private float inputSpeed;
+
+	[SerializeField]
+	private float stopTimeAfterNoInput;
+
+	[SerializeField]
+	private float transitionTimeAfterNoInput;
+
+	private float timeSinceLastInput;
 
 	void Update () 
 	{
-		transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime * speed);
+		float delta = autoSpeed;
+		float input = Input.GetAxis("Horizontal");
+
+		if (input != 0.0f) 
+		{
+			delta = input * inputSpeed;
+
+			timeSinceLastInput = stopTimeAfterNoInput + transitionTimeAfterNoInput;
+		} 
+		else if (timeSinceLastInput > 0.0f) 
+		{
+			timeSinceLastInput -= Time.deltaTime;
+
+			if (timeSinceLastInput > transitionTimeAfterNoInput)
+			{
+				delta = 0.0f;
+			}
+			else
+			{
+				delta = Mathf.Lerp(0.0f, autoSpeed, Mathf.InverseLerp(transitionTimeAfterNoInput, 0.0f, timeSinceLastInput));
+			}
+		}
+
+		transform.RotateAround(target.transform.position, Vector3.up, -Time.deltaTime * delta);
 		transform.LookAt(target.transform);
 	}
 }
